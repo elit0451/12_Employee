366fec3bd7f22c021ab804648e82b50f87dee8bc
Elitsa Marinovska <Elitsa Marinovska@DESKTOP-03FMA85>
2016-10-16 15:46:30 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace _12_Employee
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Id { get; set; }
+
+        public Employee()
+        { }
+        public Employee(string name,string type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _12_Employee
+{
+    internal class EmployeeRepository
+    {
+        List<Employee> listOfEmployees = new List<Employee>();
+        private int id = 0;
+        internal void Clear()
+        {
+            listOfEmployees.Clear();
+        }
+
+        internal int CountEmployees()
+        {
+            return listOfEmployees.Count;
+        }
+
+        internal Employee CreateEmployee(string name, string type)
+        {
+            
+            Employee newEmployee = new Employee(name, type);
+            id++;
+            newEmployee.Id = id;
+            listOfEmployees.Add(newEmployee);
+
+            return newEmployee;
+        }
+
+        internal void SaveEmployee(Employee employeeObj)
+        {
+            FileInfo file = new FileInfo("Employee" + employeeObj.Id + ".txt");
+            FileStream fs = file.Create();
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine(employeeObj.Name);
+            sw.WriteLine(employeeObj.Type);
+            sw.WriteLine(employeeObj.Id);
+            sw.Close();
+            fs.Close();
+        }
+
+        internal Employee LoadEmployee(int id)
+        {
+           return listOfEmployees[id-1];
+        }
+
+        internal List<Employee> FindAllEmployees()
+        {
+            return listOfEmployees;
+        }
+    }
+}
